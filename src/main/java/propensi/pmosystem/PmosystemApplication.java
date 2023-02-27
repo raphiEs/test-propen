133941ce9d49913f99241be42085c171dae00593
Winalton <winalton@ui.ac.id>
2023-02-27 16:26:04 +0700

    First commit

---
+++ src/main/java/propensi/pmosystem/PmosystemApplication.java
@@ -0,0 +1,13 @@
+package propensi.pmosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmosystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/propensi/pmosystem/PmosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package propensi.pmosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
