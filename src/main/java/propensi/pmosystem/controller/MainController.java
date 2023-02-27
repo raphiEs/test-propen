4f2af90fe9958d6b06c9c261195d9502cc5a8753
Winalton <winalton@ui.ac.id>
2023-02-27 20:29:49 +0700

    initial setup

---
+++ src/main/java/propensi/pmosystem/controller/MainController.java
@@ -0,0 +1,14 @@
+package propensi.pmosystem.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    
+    @RequestMapping(value = "/")
+	public String main() {
+		return "home";
+	}
+
+}
+
