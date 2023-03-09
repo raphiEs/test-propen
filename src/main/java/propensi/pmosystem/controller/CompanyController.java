d422168dbad299a810fcbefbd666a851ef1f4472
Muhammad Raffy Suprajeni <muhammad.raffy@ui.ac.id>
2023-03-09 15:50:33 +0700

    Done: PBI-4

--- src/main/java/propensi/pmosystem/controller/CompanyController.java
+++ src/main/java/propensi/pmosystem/controller/CompanyController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import propensi.pmosystem.model.BusinessModel;
 import propensi.pmosystem.model.CompanyModel;
 import propensi.pmosystem.repository.BusinessDb;
@@ -37,7 +34,6 @@ public class CompanyController {
 
         model.addAttribute("company", company);
         model.addAttribute("listBusiness", listBusiness);
-
         //log.info("Manajer memulai proses 'tambah perusahaan'");
         return "form-add-company";
     }
@@ -49,10 +45,30 @@ public class CompanyController {
         if (businessId != ""){
             company.setBusiness(businessService.getBusinessById(Long.parseLong(businessId)));
         }
-
         companyService.addCompany(company);
+
         model.addAttribute("company", company);
         //log.info("Manajer berhasil menambahkan perusahaan baru");
         return "home";
+    }
+
+    @GetMapping("/update-company/{id}")
+    public String updateCompanyFormPage(@PathVariable Long id,  Model model){
+        CompanyModel company = companyService.getCompanyById(id);
+        List<BusinessModel> listBusiness = businessService.getListBusiness();
+        BusinessModel companyBusiness = company.getBusiness();
+
+        model.addAttribute("company", company);
+        model.addAttribute("listBusiness", listBusiness);
+        model.addAttribute("companyBusiness", companyBusiness);
+        return "form-update-company";
+    }
+
+    @PostMapping("/update-company")
+    public String updateCompanySubmitPage(@ModelAttribute CompanyModel updatedCompany, @RequestParam String businessId, Model model){
+        updatedCompany.setBusiness(businessService.getBusinessById(Long.parseLong(businessId)));
+        companyService.updateCompany(updatedCompany);
+        model.addAttribute("company", updatedCompany);
+        return "/home";
     }
 }
