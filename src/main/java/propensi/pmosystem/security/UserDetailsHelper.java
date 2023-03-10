fb44ec22af503d543ecca732641f1f1785bb4da5
Winalton <winalton@ui.ac.id>
2023-03-10 11:50:30 +0700

    fix gradle dependency

--- src/main/java/propensi/pmosystem/security/UserDetailsHelper.java
+++ src/main/java/propensi/pmosystem/security/UserDetailsHelper.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.util.Collection;
 
 import org.springframework.stereotype.Service;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
