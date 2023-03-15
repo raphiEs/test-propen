9bf84158e869ab08fb465cd818d6290645186ca3
Kornelius Tarigan <kornelius.tarigan@ui.ac.id>
2023-03-15 11:36:08 +0700

    progress PBI-6,7

--- src/main/java/propensi/pmosystem/controller/CompanyController.java
+++ src/main/java/propensi/pmosystem/controller/CompanyController.java
@@ -201,39 +201,8 @@ public class CompanyController {
         model.addAttribute("loginUser", loginUser_);
         model.addAttribute("company", company);
         model.addAttribute("listProject", listProject);
-        return "form-add-company-project";
+        model.addAttribute("accessedFrom", "detailKlien");
+        return "/project/form-add-project";
     }
-
-    @PostMapping("/company/project/add")
-    public String addCompanyProjectFormSubmitPage(@ModelAttribute CompanyModel company,
-                                                  @RequestParam(value = "kodeProyeks") String[] kodeProyeks,
-                                                  @RequestParam String businessId,
-                                                  Model model){
-            //Auth
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            org.springframework.security.core.userdetails.User loginUser = (org.springframework.security.core.userdetails.User) auth.getPrincipal();
-            String username = loginUser.getUsername();
-            UserModel loginUser_ = userService.getUserByUsername(username);
-
-            //Test
-            CompanyModel updatedCompany = companyService.getCompanyById(company.getId());
-
-            //Mengisi list proyek milik klien
-            //String message = "";
-            List<ProjectModel> tempListProjectCompany = new ArrayList<>();
-            for (int i = 0 ; i<kodeProyeks.length ; i++){
-                ProjectModel tempProject = projectService.findById(Long.parseLong(kodeProyeks[i]));
-                tempProject.setCompany(updatedCompany);
-                tempListProjectCompany.add(tempProject);
-
-            }
-            updatedCompany.setProjectCompany(tempListProjectCompany);
-            updatedCompany.setBusiness(businessService.getBusinessById(Long.parseLong(businessId)));
-            companyService.updateCompany(updatedCompany);
-
-            String message = "Proyek berhasil ditambahkan pada klien '"+ company.getName() +"'";
-            model.addAttribute("loginUser", loginUser_);
-            model.addAttribute("message", message);
-            return "form-success";
-    }
+    
 }
