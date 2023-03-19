e6527a8b90cb5a78e81f81c1a9f753e879ec5b48
Oba Pakpahan <oba.pakpahan@ui.ac.id>
2023-03-19 22:21:18 +0700

    Update PBI 9 & Done AUTHORIZATION PBI-7,8,9,10 BE (oba)

---
+++ src/main/java/propensi/pmosystem/service/ProjectUserServiceImpl.java
@@ -0,0 +1,26 @@
+package propensi.pmosystem.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import propensi.pmosystem.model.ProjectModel;
+import propensi.pmosystem.model.ProjectUserModel;
+import propensi.pmosystem.repository.ProjectUserDb;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProjectUserServiceImpl implements ProjectUserService{
+    @Autowired
+    ProjectUserDb projectUserDb;
+    @Override
+    public List<ProjectUserModel> findAllByUser(Long userId) {
+        return projectUserDb.findAllByUserId(userId);
+    }
+
+    @Override
+    public ProjectUserModel addProjectUser(ProjectUserModel projectUser) {
+        return projectUserDb.save(projectUser);
+    }
+}
