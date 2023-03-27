a10e98737e98bbe78e209cf4e8eb513fcd524bff
oba20201 <obapakpahan01@gmail.com>
2023-03-27 18:02:45 +0700

    feat: add Event and Attendance Model, Add Attendance Controller, JPARepo, Service and empty template

---
+++ src/main/java/propensi/pmosystem/service/AttendanceService.java
@@ -0,0 +1,14 @@
+package propensi.pmosystem.service;
+
+import propensi.pmosystem.model.AttendanceModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AttendanceService {
+    AttendanceModel findById(String idAttendance);
+    List<AttendanceModel> findAll();
+    List<AttendanceModel> findAllByEvent(Long idEvent);
+
+    AttendanceModel addAttendance(AttendanceModel newAttendance);
+}
