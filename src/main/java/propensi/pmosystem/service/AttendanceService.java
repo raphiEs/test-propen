5ab91d60270510ad16196c467d5befb5f0798262
oba20201 <obapakpahan01@gmail.com>
2023-03-30 20:53:27 +0700

    Done PBI-26,27,37 (BE)

--- src/main/java/propensi/pmosystem/service/AttendanceService.java
+++ src/main/java/propensi/pmosystem/service/AttendanceService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceService {
-    AttendanceModel findById(String idAttendance);
+    AttendanceModel findById(Long idAttendance);
     List<AttendanceModel> findAll();
     List<AttendanceModel> findAllByEvent(Long idEvent);
 
